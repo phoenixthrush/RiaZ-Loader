e666d7cd8ce21c9081abb72edbb68dfec7b19571
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-12-04 12:30:52 +0100

    qemu explicitly specify raw image

--- RiaZ/compile.sh
+++ RiaZ/compile.sh
@@ -4,4 +4,4 @@ mkdir bin
 nasm -fbin src/payload.asm -o bin/payload.bin
 nasm -fbin src/bootldr.asm -o bin/boot.bin
 
-qemu-system-i386 bin/boot.bin
+qemu-system-i386 -drive file=bin/boot.bin,format=raw
